e5c70084a464e96fd9b316a9d479e70f14f76ea7
Vitor Fernandes Moraes <80361373+Vitor-php@users.noreply.github.com>
2022-06-04 13:33:47 -0300

    deletion of files 'Api' and 'firebaseConfig'

--- src/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-    apiKey: "AIzaSyBMr1xb_1OGclWCoIxUwfRyohbq2MkIm9c",
-    authDomain: "projeto-tcc-2b194.firebaseapp.com",
-    projectId: "projeto-tcc-2b194",
-    storageBucket: "projeto-tcc-2b194.appspot.com",
-    messagingSenderId: "821059362666",
-    appId: "1:821059362666:web:9cedd4ce521c5e93eb4f13"
-};
