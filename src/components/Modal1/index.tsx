a52d7fdeb471729a171cbec1e1f22c0b0ea920f2
Vitor Fernandes Moraes <80361373+Vitor-php@users.noreply.github.com>
2022-06-19 19:12:20 -0300

    fix: changes in modal of 'indexes'

--- src/components/Modal1/index.tsx
+++ src/components/Modal1/index.tsx
@@ -16,21 +16,23 @@ export function Modal1() {
                 <View style={styles.viewModal}>
                 <Text style={styles.modalText}>Informações</Text>
                 <Text style={styles.modalSubtitle}>
-                    Este ícone  </Text>
+                    Este ícone  <Text/>
                     <View style={viewButtonStyles({ left: 55, top: -10 }).viewButton}></View>
                     <Text style={styles.modalSubtitle}>  signifi- {`\n`}
                     ca que o índice é sobre{`\n`}</Text>
                     o assunto Renda Fixa.{`\n`}
+                    </Text>
                 <TouchableOpacity
                 onPress={() => {
                     setVisible(false);
                 }}
-            >
+                >
                 <Text></Text>
                 <AntDesign
                  name="closecircleo"
                  color='red'
                  size={25}
+                 style={styles.closeModalIcon}
                 />
             </TouchableOpacity>
                 </View>
