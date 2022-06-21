204e89402bd155d7002f3358b76684be85767301
Vitor Fernandes Moraes <80361373+Vitor-php@users.noreply.github.com>
2022-06-20 23:11:20 -0300

    fix: component historic is completed

--- src/components/Historic/index.tsx
+++ src/components/Historic/index.tsx
@@ -4,12 +4,29 @@ import { View, Text } from "react-native";
 import { styles } from "./styles";
 
 export function Historic() {
-    return (
-        <View>
-        <View style={styles.lineHistoric}></View>
-          <View style={styles.viewHistoric}>
-            <Text style={styles.textHistoric}>Histórico</Text>
-            </View>
-            </View>
-            );
+  return (
+    <View style={styles.viewHistoric}>
+      <View style={styles.lineHistoric}></View>
+      <View>
+        <View style={styles.viewHistoricInternal}>
+          <Text style={styles.textHistoric}>Histórico</Text>
+
+          <View style={styles.lineHistoric1}></View>
+          <Text style={styles.textHistoricValor1}>Valor Total: R$450.000,00 | 11/07/22</Text>
+
+          <View style={styles.lineHistoric2}></View>
+          <Text style={styles.textHistoricValor2}>Valor Total: R$45.034,00 | 12/01/22</Text>
+
+          <View style={styles.lineHistoric3}></View>
+          <Text style={styles.textHistoricValor3}>Valor Total: R$3.700 | 11/02/22</Text>
+
+          <View style={styles.lineHistoric4}></View>
+          <Text style={styles.textHistoricValor4}>Valor Total: R$2.789,32 | 12/04/22</Text>
+          
+          <View style={styles.lineHistoric5}></View>
+          <Text style={styles.textHistoricValor5}>Valor Total: R$1.500,89 | 11/05/22</Text>
+        </View>
+      </View>
+    </View>
+  );
 }
--- src/components/Historic/styles.ts
+++ src/components/Historic/styles.ts
@@ -10,12 +10,80 @@ lineHistoric: {
     backgroundColor: (theme.colors.google)
   },
   viewHistoric: {
-    width: 100,
-    height: 100
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  viewHistoricInternal: {
+    top: 30,
+    width: 250,
+    height: 158,
+    borderRadius: 15,
+    backgroundColor: (theme.colors.secondary),
   },
   textHistoric: {
-    top: 25,
+    top: 5,
     fontSize: 25,
-    fontFamily: (theme.fonts.title500)
-  }
+    fontFamily: (theme.fonts.title500),
+    marginLeft: 10
+  },
+  textHistoricValor1: {
+    top: 7,
+    margin: 2,
+    marginLeft: 5,
+    fontSize: 11.5
+  },
+  lineHistoric1: {
+    top: 5,
+    width: 250,
+    height: 1,
+    backgroundColor: (theme.colors.google),
+  },
+  textHistoricValor2: {
+    top: 13,
+    margin: 2,
+    marginLeft: 5,
+    fontSize: 11.5
+  },
+  lineHistoric2: {
+    top: 10,
+    width: 250,
+    height: 1,
+    backgroundColor: (theme.colors.google),
+  },
+  textHistoricValor3: {
+    top: 18.5,
+    margin: 2,
+    marginLeft: 5,
+    fontSize: 11.5
+  },
+  lineHistoric3: {
+    top: 15,
+    width: 250,
+    height: 1,
+    backgroundColor: (theme.colors.google),
+  },
+  textHistoricValor4: {
+    top: 22.5,
+    margin: 2,
+    marginLeft: 5,
+    fontSize: 11.5
+  },
+  lineHistoric4: {
+    top: 20,
+    width: 250,
+    height: 1,
+    backgroundColor: (theme.colors.google),
+  },
+  textHistoricValor5: {
+    top: 27,
+    margin: 2,
+    marginLeft: 5,
+    fontSize: 11.5
+  },
+  lineHistoric5: {
+    top: 25,
+    width: 250,
+    height: 1,
+    backgroundColor: (theme.colors.google),
+  },
 });
