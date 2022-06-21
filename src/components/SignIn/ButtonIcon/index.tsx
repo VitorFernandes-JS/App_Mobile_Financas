edc8929436f9e0549f7eed0d70343e5066a44b49
Vitor Fernandes Moraes <80361373+Vitor-php@users.noreply.github.com>
2022-06-21 10:13:54 -0300

    fix: code formatter

--- src/components/SignIn/ButtonIcon/index.tsx
+++ src/components/SignIn/ButtonIcon/index.tsx
@@ -1,28 +1,22 @@
-import React from 'react';
-import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
-import { View, Text, Image} from 'react-native';
-import GoogleImg from '../../../assets/google.png'
-import { styles } from './styles';
+import React from "react";
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
+import { View, Text, Image } from "react-native";
+import GoogleImg from "../../../assets/google.png";
+import { styles } from "./styles";
 
-    type Props = RectButtonProps &{
-        title: string
-    }
+type Props = RectButtonProps & {
+  title: string;
+};
 
-export function ButtonIcon( { title, ...rest } : Props){
-    return(
-        
-        <RectButton style={styles.container}
-            {...rest}
-        >
-            <View style={styles.iconWrapper}>
-            <Image source={GoogleImg} style={styles.icon}/>
-            </View>
-            <View style={styles.contentWrapper}>
-                <Text style={styles.title}>
-                    { title }
-                    </Text> 
-                    </View>
-        </RectButton>
-        
-    );
+export function ButtonIcon({ title, ...rest }: Props) {
+  return (
+    <RectButton style={styles.container} {...rest}>
+      <View style={styles.iconWrapper}>
+        <Image source={GoogleImg} style={styles.icon} />
+      </View>
+      <View style={styles.contentWrapper}>
+        <Text style={styles.title}>{title}</Text>
+      </View>
+    </RectButton>
+  );
 }
