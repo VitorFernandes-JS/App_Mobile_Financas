190f365421cdd66eca73630c253aab1249004ec7
Vitor Fernandes Moraes <80361373+Vitor-php@users.noreply.github.com>
2022-06-21 10:11:58 -0300

    fix: code formatter

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,22 +1,22 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode } from "react";
 
-import { styles } from './styles';
-import { theme } from '../../global/styles/theme';
-import { LinearGradient } from 'expo-linear-gradient';
+import { styles } from "./styles";
+import { theme } from "../../global/styles/theme";
+import { LinearGradient } from "expo-linear-gradient";
 
 type Props = {
-    children: ReactNode;
-}
+  children: ReactNode;
+};
 
 export function Background({ children }: Props) {
-    const { background , backgroundSecondary } =  theme.colors;
+  const { background, backgroundSecondary } = theme.colors;
 
-    return (
-        <LinearGradient
-        style={styles.container}
-        colors={[theme.colors.background, theme.colors.backgroundSecondary]}
-        >
-            {children}
-        </LinearGradient>
-    )
-} 
+  return (
+    <LinearGradient
+      style={styles.container}
+      colors={[theme.colors.background, theme.colors.backgroundSecondary]}
+    >
+      {children}
+    </LinearGradient>
+  );
+}
