26824ccacdae131cb68ce3e2cb1a02dec6858a90
Vitor Fernandes Moraes <80361373+Vitor-php@users.noreply.github.com>
2022-06-21 10:19:01 -0300

    fix: code formatting

--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,39 +1,39 @@
-import { StyleSheet } from 'react-native';
-import { theme } from '../../global/styles/theme';
+import { StyleSheet } from "react-native";
+import { theme } from "../../global/styles/theme";
 
 export const styles = StyleSheet.create({
-    container: {
-        height: '100%',
-        width: '100%',
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',    
-        },
-        content: {
-            marginTop: -77,
-            paddingHorizontal: 50,
-        },
-        title: {
-            color: theme.colors.heading,
-            textAlign: 'center',
-            fontSize: 40,
-            marginBottom:10,
-            fontFamily: theme.fonts.title700,
-            fontWeight: 'bold',
-            lineHeight: 40,
-            marginTop: 85,
-        },
-        image: {
-            width: 385,
-            height: 385,
-        },
-        subtitle: {
-            color: theme.colors.heading,
-            fontSize: 17,
-            fontStyle: 'italic',
-            textAlign: 'center',
-            marginBottom: 47,
-            fontFamily: theme.fonts.text500,
-            lineHeight: 24,
-        }
+  container: {
+    height: "100%",
+    width: "100%",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  content: {
+    marginTop: -77,
+    paddingHorizontal: 50,
+  },
+  title: {
+    color: theme.colors.heading,
+    textAlign: "center",
+    fontSize: 40,
+    marginBottom: 10,
+    fontFamily: theme.fonts.title700,
+    fontWeight: "bold",
+    lineHeight: 40,
+    marginTop: 85,
+  },
+  image: {
+    width: 385,
+    height: 385,
+  },
+  subtitle: {
+    color: theme.colors.heading,
+    fontSize: 17,
+    fontStyle: "italic",
+    textAlign: "center",
+    marginBottom: 47,
+    fontFamily: theme.fonts.text500,
+    lineHeight: 24,
+  },
 });
