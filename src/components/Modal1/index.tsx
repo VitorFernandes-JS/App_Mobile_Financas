37adac93117fd457503f83c5fbeef8c909a3e0df
Vitor Fernandes Moraes <80361373+Vitor-php@users.noreply.github.com>
2022-06-21 10:13:21 -0300

    fix: code formatter

--- src/components/Modal1/index.tsx
+++ src/components/Modal1/index.tsx
@@ -1,74 +1,74 @@
-import React, {useState} from 'react';
-import { View, Text, Modal, Button, TouchableOpacity} from 'react-native';
-import { styles, viewButtonStyles } from './styles'
-import { AntDesign } from '@expo/vector-icons';
+import React, { useState } from "react";
+import { View, Text, Modal, Button, TouchableOpacity } from "react-native";
+import { styles, viewButtonStyles } from "./styles";
+import { AntDesign } from "@expo/vector-icons";
 
 export function Modal1() {
-    const [visible, setVisible] = useState(false)
-    
-    return(
-        <View>
-            <Modal
-                animationType="slide"
-                transparent={true}
-                visible={visible}
-            >
-                <View style={styles.viewModal}>
-                <Text style={styles.modalText}>Informações</Text>
-                <View style={styles.line}></View>
-                <Text style={styles.modalSubtitle}>
-                    Este ícone  <Text/>
-                    <View style={viewButtonStyles({ left: 55, top: -10 }).viewButton}></View>
-                    <Text style={styles.modalSubtitle}>  signifi- {`\n`}
-                    ca que o índice é sobre{`\n`}</Text>
-                    o assunto Renda Fixa.{`\n`}
-                    </Text>
-                <TouchableOpacity
-                onPress={() => {
-                    setVisible(false);
-                }}
-                >
-                <Text></Text>
-                <AntDesign
-                 name="closecircleo"
-                 color='red'
-                 size={25}
-                 style={styles.closeModalIcon}
-                />
-            </TouchableOpacity>
-                </View>
-            </Modal>
-            
+  const [visible, setVisible] = useState(false);
 
-            <View style={viewButtonStyles({ left: 55, top: -10 }).viewButton}>
-            <TouchableOpacity
-                onPress={() => {
-                    setVisible(true);
-                }}
-            >
-                <Text style={styles.interrogation}>?</Text>
-            </TouchableOpacity>
-            </View>
-
-            <View style={viewButtonStyles({ left: -70, top: -24 }).viewButton}>
-            <TouchableOpacity
-                onPress={() => {
-                    setVisible(true);
-                }}
-            >
-                <Text style={styles.interrogation}>?</Text>
-            </TouchableOpacity>
-            </View>
-
-            <View style={viewButtonStyles({ left: 180, top: -36 }).viewButton}>
-            <TouchableOpacity
-                onPress={() => {
-                    setVisible(true);
-                }}
-            >
-                <Text style={styles.interrogation}>?</Text>
-            </TouchableOpacity>
-            </View>
+  return (
+    <View>
+      <Modal animationType="slide" transparent={true} visible={visible}>
+        <View style={styles.viewModal}>
+          <Text style={styles.modalText}>Informações</Text>
+          <View style={styles.line}></View>
+          <Text style={styles.modalSubtitle}>
+            Este ícone <Text />
+            <View
+              style={viewButtonStyles({ left: 55, top: -10 }).viewButton}
+            ></View>
+            <Text style={styles.modalSubtitle}>
+              {" "}
+              signifi- {`\n`}
+              ca que o índice é sobre{`\n`}
+            </Text>
+            o assunto Renda Fixa.{`\n`}
+          </Text>
+          <TouchableOpacity
+            onPress={() => {
+              setVisible(false);
+            }}
+          >
+            <Text></Text>
+            <AntDesign
+              name="closecircleo"
+              color="red"
+              size={25}
+              style={styles.closeModalIcon}
+            />
+          </TouchableOpacity>
         </View>
-    );
+      </Modal>
+
+      <View style={viewButtonStyles({ left: 55, top: -10 }).viewButton}>
+        <TouchableOpacity
+          onPress={() => {
+            setVisible(true);
+          }}
+        >
+          <Text style={styles.interrogation}>?</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={viewButtonStyles({ left: -70, top: -24 }).viewButton}>
+        <TouchableOpacity
+          onPress={() => {
+            setVisible(true);
+          }}
+        >
+          <Text style={styles.interrogation}>?</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={viewButtonStyles({ left: 180, top: -36 }).viewButton}>
+        <TouchableOpacity
+          onPress={() => {
+            setVisible(true);
+          }}
+        >
+          <Text style={styles.interrogation}>?</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
