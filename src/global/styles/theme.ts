2ced55bab8c560cddc16e104f6c154490d993c2f
Vitor Fernandes Moraes <80361373+Vitor-php@users.noreply.github.com>
2022-06-21 10:15:21 -0300

    fix: code formatter

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -1,20 +1,20 @@
 export const theme = {
-    colors: {
-        background: '#1DB4C5',
-        backgroundSecondary: '#65dae7',
-        heading: '#DDE3F0',
-        primary: '#ffcc00',
-        secondary:'#f2f2f2',
-        line: '#414200',
-        google: '#000',
-        buttonColor: '#ffffff',
-        buttonColor2: '#d9d9d9',
-    },
+  colors: {
+    background: "#1DB4C5",
+    backgroundSecondary: "#65dae7",
+    heading: "#DDE3F0",
+    primary: "#ffcc00",
+    secondary: "#f2f2f2",
+    line: "#414200",
+    google: "#000",
+    buttonColor: "#ffffff",
+    buttonColor2: "#d9d9d9",
+  },
 
-    fonts: {
-        title700: 'Rajdhani_700Bold',
-        title500: 'Rajdhani_500Medium',
-        text400: 'Inter_400Regular',
-        text500: 'Inter_500Medium',
-    }
+  fonts: {
+    title700: "Rajdhani_700Bold",
+    title500: "Rajdhani_500Medium",
+    text400: "Inter_400Regular",
+    text500: "Inter_500Medium",
+  },
 };
--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,46 +1,30 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { theme } from '../global/styles/theme';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { theme } from "../global/styles/theme";
 
-import { Home } from '../screens/Home';
-import { SignIn } from '../screens/SignIn';
-import { Calculator } from '../screens/Calculator';
-import { Comparator } from '../screens/Comparator';
-import { Indexes } from '../screens/Indexes';
+import { Home } from "../screens/Home";
+import { SignIn } from "../screens/SignIn";
+import { Calculator } from "../screens/Calculator";
+import { Comparator } from "../screens/Comparator";
+import { Indexes } from "../screens/Indexes";
 
 const { Navigator, Screen } = createStackNavigator();
 
 export function AuthRoutes() {
-    return(
-        <Navigator
-            screenOptions={{
-                headerShown: false,
-                cardStyle: {
-                    backgroundColor: (theme.colors.background)
-                }
-            }}
-        >
-            <Screen
-                name="SignIn"
-                component={SignIn}
-            />
-            <Screen
-                name="Home"
-                component={Home}
-            />
-            <Screen
-                name="Calculator"
-                component={Calculator}
-            />
-            <Screen
-                name="Comparator"
-                component={Comparator}
-            />
-            <Screen
-                name="Indexes"
-                component={Indexes}
-            />
-            
-        </Navigator>
-    )
+  return (
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: {
+          backgroundColor: theme.colors.background,
+        },
+      }}
+    >
+      <Screen name="SignIn" component={SignIn} />
+      <Screen name="Home" component={Home} />
+      <Screen name="Calculator" component={Calculator} />
+      <Screen name="Comparator" component={Comparator} />
+      <Screen name="Indexes" component={Indexes} />
+    </Navigator>
+  );
 }
