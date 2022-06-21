192fa2add4642da493a467591c9a8aa658f54f82
Vitor Fernandes Moraes <80361373+Vitor-php@users.noreply.github.com>
2022-06-21 10:12:16 -0300

    fix: code formatter

--- src/components/Background/styles.ts
+++ src/components/Background/styles.ts
@@ -1,8 +1,8 @@
-import { StyleSheet } from 'react-native'
-import { theme } from '../../global/styles/theme';
+import { StyleSheet } from "react-native";
+import { theme } from "../../global/styles/theme";
 
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    }
+  container: {
+    flex: 1,
+  },
 });
