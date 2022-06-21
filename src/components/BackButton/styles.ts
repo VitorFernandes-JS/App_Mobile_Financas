c1da259f28f3c17eef87aa0ecabe5e33e98744f2
Vitor Fernandes Moraes <80361373+Vitor-php@users.noreply.github.com>
2022-06-21 10:11:43 -0300

    fix: code formatter

--- src/components/BackButton/styles.ts
+++ src/components/BackButton/styles.ts
@@ -1,21 +1,21 @@
-import { StyleSheet} from 'react-native';
-import { theme } from '../../global/styles/theme';
+import { StyleSheet } from "react-native";
+import { theme } from "../../global/styles/theme";
 
 export const styles = StyleSheet.create({
-    container: {
-        height: 47,
-        width: 47,
-        borderRadius: 50,
-        alignItems: 'flex-start',
-        marginTop: -45,
-        right: 135
-    },
-    borderBackButton: {
-        height: 58,
-        width: 150,
-        borderRadius: 50,
-        marginTop: 45,
-        backgroundColor: '#fff',
-        right: 225
-    }
+  container: {
+    height: 47,
+    width: 47,
+    borderRadius: 50,
+    alignItems: "flex-start",
+    marginTop: -45,
+    right: 135,
+  },
+  borderBackButton: {
+    height: 58,
+    width: 150,
+    borderRadius: 50,
+    marginTop: 45,
+    backgroundColor: "#fff",
+    right: 225,
+  },
 });
