71a93c7c3e5a78437f3fe2cd87104a9163b76b13
Vitor Fernandes Moraes <80361373+Vitor-php@users.noreply.github.com>
2022-06-21 21:39:33 -0300

    fix: refatoring code

--- App.tsx
+++ App.tsx
@@ -2,10 +2,7 @@ import React from "react";
 import { StatusBar } from "react-native";
 import { useFonts } from "expo-font";
 import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
-import {
-  Rajdhani_500Medium,
-  Rajdhani_700Bold,
-} from "@expo-google-fonts/rajdhani";
+import { Rajdhani_500Medium, Rajdhani_700Bold } from "@expo-google-fonts/rajdhani";
 import AppLoading from "expo-app-loading";
 
 import { Routes } from "./src/routes";
