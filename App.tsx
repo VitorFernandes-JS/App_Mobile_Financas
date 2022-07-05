d242f4ff3fc37d751feb2d3a9a34c1c8b6e102a3
Vitor Fernandes Moraes <80361373+Vitor-php@users.noreply.github.com>
2022-07-05 13:43:42 -0300

    fix: alterating front-end

--- App.tsx
+++ App.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { StatusBar } from "react-native";
 import { useFonts } from "expo-font";
-import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
-import { Rajdhani_500Medium, Rajdhani_700Bold } from "@expo-google-fonts/rajdhani";
+import { Raleway_100Thin, Raleway_200ExtraLight, Raleway_300Light, Raleway_400Regular, Raleway_500Medium, Raleway_600SemiBold, Raleway_700Bold, Raleway_800ExtraBold, Raleway_900Black  } from "@expo-google-fonts/raleway";
 import AppLoading from "expo-app-loading";
 
 import { Routes } from "./src/routes";
@@ -10,10 +9,15 @@ import { Background } from "./src/components/Background";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    Inter_400Regular,
-    Inter_500Medium,
-    Rajdhani_500Medium,
-    Rajdhani_700Bold,
+    Raleway_100Thin, 
+    Raleway_200ExtraLight, 
+    Raleway_300Light, 
+    Raleway_400Regular, 
+    Raleway_500Medium, 
+    Raleway_600SemiBold, 
+    Raleway_700Bold, 
+    Raleway_800ExtraBold, 
+    Raleway_900Black
   });
 
   if (!fontsLoaded) {
--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -12,6 +12,5 @@ export const theme = {
     title700: "Rajdhani_700Bold",
     title500: "Rajdhani_500Medium",
     text400: "Inter_400Regular",
-    text500: "Inter_500Medium",
   },
 };
