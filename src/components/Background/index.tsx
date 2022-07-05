7058f343eb20e006e05f1590b3439539c637c6a4
Vitor Fernandes Moraes <80361373+Vitor-php@users.noreply.github.com>
2022-07-05 13:20:09 -0300

    fix: alterating front-end

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from "react";
-import View from "react-native";
+import { View } from "react-native";
 import { styles } from "./styles";
 import { theme } from "../../global/styles/theme";
 
@@ -8,12 +8,10 @@ type Props = {
 };
 
 export function Background({ children }: Props) {
-  const { background, backgroundSecondary } = theme.colors;
 
   return (
     <View
       style={styles.container}
-      colors={[theme.colors.background, theme.colors.backgroundSecondary]}
     >
       {children}
       </View>
