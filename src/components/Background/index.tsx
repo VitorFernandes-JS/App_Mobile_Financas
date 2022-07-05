489eeb0580d10850f04de0aa110f438010508603
Vitor Fernandes Moraes <80361373+Vitor-php@users.noreply.github.com>
2022-07-05 13:17:01 -0300

    fix: historic component deletion

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,8 +1,7 @@
 import React, { ReactNode } from "react";
-
+import View from "react-native";
 import { styles } from "./styles";
 import { theme } from "../../global/styles/theme";
-import { LinearGradient } from "expo-linear-gradient";
 
 type Props = {
   children: ReactNode;
@@ -12,11 +11,11 @@ export function Background({ children }: Props) {
   const { background, backgroundSecondary } = theme.colors;
 
   return (
-    <LinearGradient
+    <View
       style={styles.container}
       colors={[theme.colors.background, theme.colors.backgroundSecondary]}
     >
       {children}
-    </LinearGradient>
+      </View>
   );
 }
