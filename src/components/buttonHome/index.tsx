c1942ae2bd4d66f53ea78712fa1242e0f709079f
Vitor-php <vitorfernandesmoraes01@gmail.com>
2022-07-28 00:40:51 -0300

    fix: formatting code

--- src/components/buttonHome/index.tsx
+++ src/components/buttonHome/index.tsx
@@ -4,10 +4,15 @@ import {
   Text,
   Image,
   Pressable,
+  Button,
 } from "react-native";
 import { styles } from "./styles";
 import { useNavigation } from "@react-navigation/native";
 
 export function buttonHome() {
-    <SafeAreaView></SafeAreaView>
+    <SafeAreaView>
+        <Button title="teste" onPress={}>
+        <Text></Text>
+        </Button>
+    </SafeAreaView>
 }
