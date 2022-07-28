c939717d0b66dfc6f9ef5ebef955be0f7434cb11
Vitor-php <vitorfernandesmoraes01@gmail.com>
2022-07-27 23:56:28 -0300

    feat: baseboard is completed

--- src/components/baseboard/index.tsx
+++ src/components/baseboard/index.tsx
@@ -1,33 +1,58 @@
 import React from "react";
-import { SafeAreaView, Text, TouchableOpacity, Image} from "react-native";
+import {
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  Image,
+  Pressable,
+} from "react-native";
 import { styles } from "./styles";
+import { useNavigation } from "@react-navigation/native";
 
-import iconHome from "../../assets/icon_home.png";
-import iconPig from "../../assets/icon_pig.png";
+import { AntDesign } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+
 import star from "../../assets/star.png";
-import iconHeart from "../../assets/icon_heart.png";
-import iconProfile from "../../assets/icon_perfil.png";
 
-export function Baseboard() {
-
-    return(
-        <SafeAreaView style={styles.container}>
-            <TouchableOpacity>
-                <Image source={iconHome} style={styles.iconHome}/>
-                <Text style={styles.textHome}>Menu</Text>
-            </TouchableOpacity>
-            <TouchableOpacity>
-                <Image source={iconPig} style={styles.iconPig}/>
-                <Text style={styles.textHome}>Menu</Text>
-            </TouchableOpacity>
-            <TouchableOpacity>
-                <Image source={iconHeart} style={styles.iconHeart}/>
-                <Text style={styles.textHome}>Menu</Text>
-            </TouchableOpacity>
-            <TouchableOpacity>
-                <Image source={iconProfile} style={styles.iconProfile}/>
-                <Text style={styles.textHome}>Menu</Text>
-            </TouchableOpacity>
-        </SafeAreaView>
-    )
+type Params = {
+  token: string;
 };
+
+export function Baseboard({ token }: Params) {
+  const navigation = useNavigation();
+
+  function handleHome() {
+    navigation.navigate("Home", { token });
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Pressable onPress={handleHome} style={styles.iconHome}>
+        <AntDesign name="home" size={40} style={styles.iconHome} />
+        <Text style={styles.textHome}>Menu</Text>
+      </Pressable>
+
+      <Pressable>
+        <Ionicons name="wallet-outline" size={40} style={styles.iconWallet} />
+        <Text style={styles.textWallet}>Carteira</Text>
+      </Pressable>
+
+      <Pressable>
+        <SafeAreaView style={styles.borderStar}/>
+            <Image source={star} style={styles.iconStar}/>
+            <Text style={styles.textGoal}>Metas</Text>
+        <SafeAreaView style={styles.borderStar1}/>
+      </Pressable>
+
+      <Pressable>
+        <AntDesign name="hearto" size={40} style={styles.iconFavorite}/>
+        <Text style={styles.textFavorite}>Favoritos</Text>
+      </Pressable>
+
+      <Pressable>
+        <AntDesign name="user" size={40} style={styles.iconProfile}/>
+        <Text style={styles.textProfile}>Perfil</Text>
+      </Pressable>
+    </SafeAreaView>
+  );
+}
--- src/components/baseboard/styles.ts
+++ src/components/baseboard/styles.ts
@@ -12,22 +12,85 @@ export const styles = StyleSheet.create({
         shadowOffset: {width: 1, height: 1},
     },
     iconHome: {
-        width: 40,
-        height: 40,
-        top: 15,
-        marginLeft: 15
+        opacity: 0.8,
+        top: 5,
+        marginLeft: 17,
+        color: theme.colors.color1
     },
     textHome: {
-        top: 10,
-        marginLeft: 19,
+        top: 7,
+        marginLeft: 20,
         color: theme.colors.color1,
         fontSize: 12,
         fontFamily: theme.fonts.font4_regular
     },
-    iconPig: {
-        width: 40,
-        height: 40,
-        top: -37,
-        marginLeft: 80
+    iconWallet: {
+        top: -45,
+        marginLeft: 110,
+        color: theme.colors.color1,
+        opacity: 0.6,
     },
+    textWallet: {
+        top: -44,
+        marginLeft: 109,
+        color: theme.colors.color1,
+        fontSize: 12,
+        fontFamily: theme.fonts.font4_regular
+    },
+    borderStar: {
+        width: 60,
+        height: 60,
+        backgroundColor: theme.colors.color2,
+        borderRadius: 50,
+        top: -140,
+        marginLeft: 175
+    },
+    borderStar1: {
+        width: 50,
+        height: 50,
+        backgroundColor: theme.colors.color6,
+        borderRadius: 50,
+        top: -246,
+        marginLeft: 180
+    },
+    iconStar: {
+        width: 37,
+        height: 37,
+        top: -190,
+        marginLeft: 187,
+        zIndex: 1
+    },
+    textGoal: {
+        top: -170,
+        marginLeft: 188,
+        color: theme.colors.color1,
+        fontSize: 12,
+        fontFamily: theme.fonts.font4_regular
+    },
+    iconFavorite: {
+        top: -260,
+        marginLeft: 255,
+        color: theme.colors.color1,
+        opacity: 0.6,
+    },
+    textFavorite: {
+        top: -260,
+        marginLeft: 250,
+        color: theme.colors.color1,
+        fontSize: 12,
+        fontFamily: theme.fonts.font4_regular
+    },
+    iconProfile: {
+        top: -315.5,
+        marginLeft: 340,
+        color: theme.colors.color1,
+        opacity: 0.6,
+    },
+    textProfile: {
+        top: -313.8,
+        marginLeft: 347,
+        color: theme.colors.color1,
+        fontSize: 12,
+        fontFamily: theme.fonts.font4_regular
+    }
 });
