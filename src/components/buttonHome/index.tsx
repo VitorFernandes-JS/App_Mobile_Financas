4d40e0156de78674db69763d2b50f87baa4c0f83
Vitor-php <vitorfernandesmoraes01@gmail.com>
2022-07-28 00:24:16 -0300

    fix: formatting code

---
+++ src/components/buttonHome/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  Image,
+  Pressable,
+} from "react-native";
+import { styles } from "./styles";
+import { useNavigation } from "@react-navigation/native";
