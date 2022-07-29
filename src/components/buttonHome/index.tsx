b74a117a05dde7993348e05b14da3a3b2b5cf4da
Vitor-php <vitorfernandesmoraes01@gmail.com>
2022-07-29 20:49:09 -0300

    fix: formatting code

--- src/components/buttonHome/index.tsx
+++ src/components/buttonHome/index.tsx
@@ -4,14 +4,14 @@ import { RectButton, RectButtonProps } from "react-native-gesture-handler";
 import { styles, viewButtonStyles } from "./styles";
 import { useNavigation } from "@react-navigation/native";
 
-import { CalculatorImg } from "../../assets/icon_calculadora.png"
+import { CalculatorImg } from "../../assets/icon_calculadora.png";
 
 interface IButtonHomeProps {
   token: string;
   img: any;
 }
 
-export function ButtonHome({ token, img , ...rest }: IButtonHomeProps) {
+export function ButtonHome({ token, img, ...rest }: IButtonHomeProps) {
   const navigation = useNavigation();
 
   function CalculatorRote() {
@@ -19,14 +19,19 @@ export function ButtonHome({ token, img , ...rest }: IButtonHomeProps) {
   }
   return (
     <SafeAreaView style={styles.container}>
-      <RectButton onPress={CalculatorRote} style={viewButtonStyles({ left: -60, top: -80}).button} {...rest}>
+      <RectButton
+        onPress={CalculatorRote}
+        style={viewButtonStyles({ left: -60, top: -80 }).button}
+        {...rest}
+      >
         <SafeAreaView>
-        <Image source={CalculatorImg}></Image>
+          <Image source={CalculatorImg}></Image>
         </SafeAreaView>
       </RectButton>
-      <RectButton style={viewButtonStyles({ left: 60, top: -179.5}).button} {...rest}>
-      </RectButton>
-      
+      <RectButton
+        style={viewButtonStyles({ left: 60, top: -179.5 }).button}
+        {...rest}
+      ></RectButton>
     </SafeAreaView>
   );
 }
