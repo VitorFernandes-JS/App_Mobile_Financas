da3df3a211e6b12fd9aeb95c30e1a6ef82acd382
Vitor-php <vitorfernandesmoraes01@gmail.com>
2022-07-29 22:37:56 -0300

    fix: formatting code

---
+++ src/components/modalPattern/index.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { SafeAreaView, Text, Modal } from "react-native";
+import { styles, viewButtonStyles } from "./styles";
+import { RectButton } from "react-native-gesture-handler";
+
+import { AntDesign } from "@expo/vector-icons";
+
+export function Modal1() {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <SafeAreaView>
+      <Modal animationType="slide" transparent={true} visible={visible}>
+        <SafeAreaView style={styles.viewModal}>
+          <Text style={styles.modalText}>Informações</Text>
+          <SafeAreaView style={styles.line}></SafeAreaView>
+          <Text style={styles.modalSubtitle}>
+            Este ícone <Text />
+            <SafeAreaView
+              style={viewButtonStyles({ left: 55, top: -10 }).viewButton}
+            ></SafeAreaView>
+            <Text style={styles.modalSubtitle}>
+              {" "}
+              signifi- {`\n`}
+              ca que o índice é sobre{`\n`}
+            </Text>
+            o assunto Renda Fixa.{`\n`}
+          </Text>
+          <RectButton
+            onPress={() => {
+              setVisible(false);
+            }}
+          >
+            <Text></Text>
+            <AntDesign
+              name="closecircleo"
+              color="red"
+              size={25}
+              style={styles.closeModalIcon}
+            />
+          </RectButton>
+        </SafeAreaView>
+      </Modal>
+      </SafeAreaView>
+  )};
