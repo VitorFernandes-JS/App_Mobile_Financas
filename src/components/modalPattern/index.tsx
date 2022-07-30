00e41b796f0601c742e2ad229fe3b12b0f375d5d
Vitor-php <vitorfernandesmoraes01@gmail.com>
2022-07-29 22:38:15 -0300

    fix: formatting code

--- src/components/modalPattern/index.tsx
+++ src/components/modalPattern/index.tsx
@@ -6,40 +6,41 @@ import { RectButton } from "react-native-gesture-handler";
 import { AntDesign } from "@expo/vector-icons";
 
 export function Modal1() {
-  const [visible, setVisible] = useState(false);
+    const [visible, setVisible] = useState(false);
 
-  return (
-    <SafeAreaView>
-      <Modal animationType="slide" transparent={true} visible={visible}>
-        <SafeAreaView style={styles.viewModal}>
-          <Text style={styles.modalText}>Informações</Text>
-          <SafeAreaView style={styles.line}></SafeAreaView>
-          <Text style={styles.modalSubtitle}>
-            Este ícone <Text />
-            <SafeAreaView
-              style={viewButtonStyles({ left: 55, top: -10 }).viewButton}
-            ></SafeAreaView>
-            <Text style={styles.modalSubtitle}>
-              {" "}
-              signifi- {`\n`}
-              ca que o índice é sobre{`\n`}
-            </Text>
-            o assunto Renda Fixa.{`\n`}
-          </Text>
-          <RectButton
-            onPress={() => {
-              setVisible(false);
-            }}
-          >
-            <Text></Text>
-            <AntDesign
-              name="closecircleo"
-              color="red"
-              size={25}
-              style={styles.closeModalIcon}
-            />
-          </RectButton>
+    return (
+        <SafeAreaView>
+            <Modal animationType="slide" transparent={true} visible={visible}>
+                <SafeAreaView style={styles.viewModal}>
+                    <Text style={styles.modalText}>Informações</Text>
+                    <SafeAreaView style={styles.line}></SafeAreaView>
+                    <Text style={styles.modalSubtitle}>
+                        Este ícone <Text />
+                        <SafeAreaView
+                            style={viewButtonStyles({ left: 55, top: -10 }).viewButton}
+                        ></SafeAreaView>
+                        <Text style={styles.modalSubtitle}>
+                            {" "}
+                            signifi- {`\n`}
+                            ca que o índice é sobre{`\n`}
+                        </Text>
+                        o assunto Renda Fixa.{`\n`}
+                    </Text>
+                    <RectButton
+                        onPress={() => {
+                            setVisible(false);
+                        }}
+                    >
+                        <Text></Text>
+                        <AntDesign
+                            name="closecircleo"
+                            color="red"
+                            size={25}
+                            style={styles.closeModalIcon}
+                        />
+                    </RectButton>
+                </SafeAreaView>
+            </Modal>
         </SafeAreaView>
-      </Modal>
-      </SafeAreaView>
-  )};
+    )
+};
