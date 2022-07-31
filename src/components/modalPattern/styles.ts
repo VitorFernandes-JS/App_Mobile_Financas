32068b3532605888d9d82993d49062f133fc20c3
Vitor-php <vitorfernandesmoraes01@gmail.com>
2022-07-31 17:14:53 -0300

    fix: formatting code

--- src/components/modalPattern/styles.ts
+++ src/components/modalPattern/styles.ts
@@ -1,52 +1,47 @@
 import { StyleSheet } from "react-native";
 import { theme } from "../../global/styles/theme";
 
-
 export const styles = StyleSheet.create({
-
-    viewModal: {
-        width: 200,
-        height: 200,
-        backgroundColor: theme.colors.color6,
-        borderRadius: 15,
-        top: '30%',
-        left: '26%',
-        shadowOpacity: 0.7,
-      shadowOffset: {width: 2, height: 2},
-    },
-    interrogation: {
-        width: 20,
-        height: 20,
-        top: 53
-    },
-    interrogation1: {
-        width: 20,
-        height: 20,
-        top: -1,
-        opacity: 0.6
-    },
-    modalText: {
-        fontFamily: theme.fonts.font5_medium,
-        fontSize: 20,
-        textAlign: 'center',
-        top: 5
-    },
-    line: {
-        width: 200,
-        height: 1.5,
-        top: 7,
-        backgroundColor: theme.colors.color3,
-      },
-    modalSubtitle: {
-        margin: 10,
-        top: 10,
-        fontFamily: theme.fonts.font4_regular
-    },
-    rectButtoncloseModalIcon: {
-        top: 65,
-        left: '45%'
-    },
-    
-   
+  viewModal: {
+    width: 200,
+    height: 200,
+    backgroundColor: theme.colors.color6,
+    borderRadius: 15,
+    top: "30%",
+    left: "26%",
+    shadowOpacity: 0.7,
+    shadowOffset: { width: 2, height: 2 },
+  },
+  interrogation: {
+    width: 20,
+    height: 20,
+    top: 53,
+  },
+  interrogation1: {
+    width: 20,
+    height: 20,
+    top: -1,
+    opacity: 0.6,
+  },
+  modalText: {
+    fontFamily: theme.fonts.font5_medium,
+    fontSize: 20,
+    textAlign: "center",
+    top: 5,
+  },
+  line: {
+    width: 200,
+    height: 1.5,
+    top: 7,
+    backgroundColor: theme.colors.color3,
+  },
+  modalSubtitle: {
+    margin: 10,
+    top: 10,
+    fontFamily: theme.fonts.font4_regular,
+  },
+  rectButtoncloseModalIcon: {
+    top: 65,
+    left: "45%",
+  },
 });
-
