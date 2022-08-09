a8faba53fb5d25b8aa3c7ad955523cc9af5ba993
Vitor-php <vitorfernandesmoraes01@gmail.com>
2022-08-09 14:01:45 -0300

    fix: formatting code

--- src/screens/Wallet/styles.ts
+++ src/screens/Wallet/styles.ts
@@ -2,22 +2,22 @@ import { StyleSheet } from "react-native";
 import { theme } from "../../global/styles/theme";
 
 export const styles = StyleSheet.create({
-    container: {
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    box: {
-        width: 300,
-        height: 100,
-        backgroundColor: theme.colors.color6,
-        borderRadius: 15,
-        top: -90,
-        shadowOpacity: 0.3,
-        shadowOffset: {width: 1,height: 3}
-    },
-    textEnter: {
-        margin: 10,
-        color: '#26B602',
-        fontFamily: theme.fonts.font4_regular
-    }
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  box: {
+    width: 300,
+    height: 100,
+    backgroundColor: theme.colors.color6,
+    borderRadius: 15,
+    top: -90,
+    shadowOpacity: 0.3,
+    shadowOffset: { width: 1, height: 3 },
+  },
+  textEnter: {
+    margin: 10,
+    color: "#26B602",
+    fontFamily: theme.fonts.font4_regular,
+  },
 });
