a684f06f28530ee3b5642607f03b3b049566824d
Vitor-php <vitorfernandesmoraes01@gmail.com>
2022-08-09 16:58:53 -0300

    fix: texts in box complete

--- src/screens/Wallet/index.tsx
+++ src/screens/Wallet/index.tsx
@@ -16,14 +16,16 @@ export function Wallet({ token }: IWalletProps) {
     <SafeAreaView style={styles.container}>
       <HeaderWallet
         text="Carteira "
-        value="1000,00"
+        value="100.000,00"
         token={token}
         img={WalletImg}
       />
       <SafeAreaView style={styles.box}>
-        <Text style={styles.textEnter}>Entrada</Text>
+        <Text style={styles.textEnter}>Entrada:</Text>
         <Text style={styles.valueOpen}>R$ 4.000,00</Text>
         <SafeAreaView style={styles.line}/>
+        <Text style={styles.textLeft}>Saída:</Text>
+        <Text style={styles.valueLeft}>R$ 10.000,00</Text>
       </SafeAreaView>
       <Baseboard token={token} />
     </SafeAreaView>
