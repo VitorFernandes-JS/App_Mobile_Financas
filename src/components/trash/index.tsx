3cc6e4c0e3e538c395b1185d564681f30ecd9c93
Vitor-php <vitorfernandesmoraes01@gmail.com>
2022-08-12 12:13:21 -0300

    fix: formatted code

--- src/components/trash/index.tsx
+++ src/components/trash/index.tsx
@@ -1,29 +1,18 @@
 import React from "react";
-import {
-  SafeAreaView,
-} from "react-native";
+import { SafeAreaView } from "react-native";
 import { RectButton } from "react-native-gesture-handler";
 import { styles } from "./styles";
-
 
 import { EvilIcons } from "@expo/vector-icons";
 
 export function Trash() {
-
-    return(
-        <SafeAreaView style={styles.container}>
-            <RectButton>
-                <SafeAreaView style={styles.body}>
-                    <EvilIcons
-                        name="trash"
-                        size={22}
-                        style={styles.iconTrash}
-                    />
-                </SafeAreaView>
-            </RectButton>
+  return (
+    <SafeAreaView style={styles.container}>
+      <RectButton>
+        <SafeAreaView style={styles.body}>
+          <EvilIcons name="trash" size={22} style={styles.iconTrash} />
         </SafeAreaView>
-    ) 
-
+      </RectButton>
+    </SafeAreaView>
+  );
 }
-
-
