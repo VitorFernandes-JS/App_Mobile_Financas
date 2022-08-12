002f26d52333d13054361aa7e9617f68c3a6a5ab
Vitor-php <vitorfernandesmoraes01@gmail.com>
2022-08-12 12:38:09 -0300

    fix: formatting code

--- src/components/add/styles.ts
+++ src/components/add/styles.ts
@@ -2,21 +2,21 @@ import { StyleSheet } from "react-native";
 import { theme } from "../../global/styles/theme";
 
 export const styles = StyleSheet.create({
-    container: {
-        width: "100%",
-        alignItems: "center",
-        justifyContent: "center",
-      },
-      body: {
-        width: 25,
-        height: 25,
-        backgroundColor: "#26B602",
-        borderRadius: 5,
-        alignItems: "center",
-        justifyContent: "center",
-      },
-      iconTrash: {
-        color: theme.colors.color6,
-        left: 1
-      },
+  container: {
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  body: {
+    width: 25,
+    height: 25,
+    backgroundColor: "#26B602",
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  iconTrash: {
+    color: theme.colors.color6,
+    left: 1,
+  },
 });
