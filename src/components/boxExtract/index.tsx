7a9aa441ee18c0b7f9f7edb3bad379c032eba63e
Vitor-php <vitorfernandesmoraes01@gmail.com>
2022-08-12 12:40:50 -0300

    fix: formatting code

--- src/components/boxExtract/index.tsx
+++ src/components/boxExtract/index.tsx
@@ -3,14 +3,11 @@ import { SafeAreaView, Text } from "react-native";
 import { styles } from "./styles";
 
 export function BoxExtract() {
-
-    return(
-        <SafeAreaView style={styles.container}>
-            <SafeAreaView style={styles.box}>
-                <Text>
-
-                </Text>
-            </SafeAreaView>
-        </SafeAreaView>
-    )
+  return (
+    <SafeAreaView style={styles.container}>
+      <SafeAreaView style={styles.box}>
+        <Text></Text>
+      </SafeAreaView>
+    </SafeAreaView>
+  );
 }
