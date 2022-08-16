f97a804f2bbdfb26fe04afdcb30399e46710dcd3
Vitor-php <vitorfernandesmoraes01@gmail.com>
2022-08-16 20:41:20 -0300

    fix: formating code

--- src/components/textField/index.tsx
+++ src/components/textField/index.tsx
@@ -3,24 +3,20 @@ import { SafeAreaView, Text, TextInput } from "react-native";
 import { styles } from "./styles";
 
 export function TextField() {
-
   return (
     <SafeAreaView style={styles.container}>
-
       <SafeAreaView style={styles.viewValue}>
-      <SafeAreaView style={styles.boxGreen}>
-        <Text style={styles.textTitleValue}>Valor</Text>
+        <SafeAreaView style={styles.boxGreen}>
+          <Text style={styles.textTitleValue}>Valor</Text>
+        </SafeAreaView>
+        <SafeAreaView style={styles.boxWhite}>
+          <TextInput
+            style={styles.textInputValue}
+            placeholder="R$00,00"
+            placeholderTextColor={"#808080"}
+          ></TextInput>
+        </SafeAreaView>
       </SafeAreaView>
-      <SafeAreaView style={styles.boxWhite}>
-        <TextInput 
-        style={styles.textInputValue}
-        placeholder="R$00,00"
-        placeholderTextColor={"#808080"} 
-        >
-        </TextInput>
-      </SafeAreaView>
-      </SafeAreaView>
-
     </SafeAreaView>
   );
 }
