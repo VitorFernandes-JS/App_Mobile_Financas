cacceb55636e7b6e74061ff287a44eee5e5bae33
Vitor-php <vitorfernandesmoraes01@gmail.com>
2022-08-17 17:58:23 -0300

    feat: create screen Wallet_Investment

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -9,10 +9,11 @@ import { Comparator } from "../screens/Comparator";
 import { Indexes } from "../screens/Indexes";
 import { Profile } from "../screens/Profile";
 import { Wallet } from "../screens/Wallet";
-import { Favorite } from "../screens/Favorite";
-import { Financing } from "../screens/Financing";
-import { Videos } from "../screens/Videos";
-import { Loan } from "../screens/Loan";
+import { Wallet_Investment } from "../screens/Wallet_Investment";
+// import { Favorite } from "../screens/Favorite";
+// import { Financing } from "../screens/Financing";
+// import { Videos } from "../screens/Videos";
+// import { Loan } from "../screens/Loan";
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -33,6 +34,7 @@ export function AuthRoutes() {
       <Screen name="Comparator" component={Comparator} />
       <Screen name="Indexes" component={Indexes} />
       <Screen name="Wallet" component={Wallet} />
+      <Screen name="WalletInvestment" component={Wallet_Investment} />
     </Navigator>
   );
 }
---
+++ src/screens/Wallet_Investment/index.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+import { styles } from "./styles";
+
+import WalletInvestmentImg from "../../assets/grafico-de-pizza.png";
+
+import { HeaderWallet } from "../../components/headerWallet";
+import { Baseboard } from "../../components/baseboard";
+import { BoxExtract } from "../../components/boxExtract";
+import { Trash } from "../../components/trash";
+import { Add } from "../../components/add";
+
+interface IWalletProps {
+  token: string;
+}
+
+export function Wallet_Investment({ token }: IWalletProps) {
+  return (
+    <SafeAreaView style={styles.container}>
+      <HeaderWallet
+        text="Carteira "
+        value="50.000,00"
+        token={token}
+        img={WalletInvestmentImg}
+      />
+      <SafeAreaView style={styles.box}>
+        <Text style={styles.textEnter}>Entrada:</Text>
+        <Text style={styles.valueOpen}>R$ 4.000,00</Text>
+        <SafeAreaView style={styles.line} />
+        <Text style={styles.textLeft}>Saída:</Text>
+        <Text style={styles.valueLeft}>R$ 10.000,00</Text>
+      </SafeAreaView>
+      <SafeAreaView style={styles.viewTrash}>
+        <Trash />
+      </SafeAreaView>
+      <SafeAreaView style={styles.viewAdd}>
+        <Add/>
+      </SafeAreaView>
+      <SafeAreaView style={styles.body}>
+        <BoxExtract />
+      </SafeAreaView>
+      <Baseboard token={token} />
+    </SafeAreaView>
+  );
+}
