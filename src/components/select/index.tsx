5de81c37a4ef0e99090536d22e8c86c45ece81eb
Vitor-php <vitorfernandesmoraes01@gmail.com>
2022-08-23 14:45:01 -0300

    fix: changes in select

---
+++ src/components/select/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+import styles from "./styles";
+
+export default class index extends Component {
+  render() {
+    return (
+      <View>
+        <Text>index</Text>
+      </View>
+    )
+  }
+}
