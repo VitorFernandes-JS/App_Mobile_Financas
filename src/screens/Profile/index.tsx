a35e89673a8aa652fa9fd96de501dd449698bd15
Vitor-php <vitorfernandesmoraes01@gmail.com>
2022-08-23 10:22:09 -0300

    fix: profile code changes

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -4,67 +4,52 @@ import { styles } from "./style";
 import { useNavigation } from "@react-navigation/native";
 
 interface IProfileProps {
-    route: any;
-    children: ReactNode;
-  }
+  route: any;
+  children: ReactNode;
+}
 
-  type Params = {
-    token: string;
-  };
-  
-  type Profile = {
-    email: string;
-    family_name: string;
-    given_name: string;
-    locale: string;
-    name: string;
-    picture: string;
-  };
+type Params = {
+  token: string;
+};
+
+type Profile = {
+  email: string;
+  family_name: string;
+  given_name: string;
+  locale: string;
+  name: string;
+  picture: string;
+};
 
 export function Profile({ route }: IProfileProps) {
-    const navigation = useNavigation();
-    const [profile, setProfile] = useState({} as Profile);
-    const { token } = route.params;
-    
-    async function loadProfile() {
-        const response = await fetch(
-          `https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token=${token}`
-        );
-        const userInfo = await response.json();
-        setProfile(userInfo);
-      }
+  const navigation = useNavigation();
+  const [profile, setProfile] = useState({} as Profile);
+  const { token } = route.params;
 
-    function handleHome() {
-        navigation.navigate("Home", { token });
-      }
-
-      useEffect(() => {
-        loadProfile();
-      }, []);
-
-    return (
-        <SafeAreaView style={styles.container}>
-            <SafeAreaView style={styles.backButton}>
-            <BackButton onPress={handleHome}/>
-            </SafeAreaView>
-            <SafeAreaView style={styles.border}>
-            <Image source={{ uri: profile.picture }} style={[styles.avatar]} />
-            <SafeAreaView style={styles.viewGivenName}>
-            <Text style={styles.givenName}>Bem-vindo, {profile.given_name}!</Text>
-            </SafeAreaView>
-            <SafeAreaView style={styles.name}>
-              <Text style={styles.textName}>Nome: {profile.given_name}</Text>
-            </SafeAreaView>
-            <SafeAreaView style={styles.familyName}>
-              <Text style={styles.textFamilyName}>Sobrenome: {profile.family_name}</Text>
-            </SafeAreaView>
-            <SafeAreaView style={styles.email}>
-              <Text style={styles.textEmail}>E-mail: {profile.email}</Text>
-            </SafeAreaView>
-            <SafeAreaView style={styles.location}>
-              <Text style={styles.textLocation}>Localização: {profile.locale}</Text>
-            </SafeAreaView>
-            </SafeAreaView>
-        </SafeAreaView>
+  async function loadProfile() {
+    const response = await fetch(
+      `https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token=${token}`
     );
-};
+    const userInfo = await response.json();
+    setProfile(userInfo);
+  }
+  console.warn(token)
+
+  function handleHome() {
+    navigation.navigate("Home", { token });
+  }
+
+  useEffect(() => {
+    loadProfile();
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <SafeAreaView style={styles.header}>
+        <SafeAreaView>
+          <Image source={{ uri: profile.picture }} style={styles.avatar}></Image>
+        </SafeAreaView>
+      </SafeAreaView>
+    </SafeAreaView>
+  )
+}
--- src/screens/Profile/style.ts
+++ src/screens/Profile/style.ts
@@ -7,106 +7,18 @@ export const styles = StyleSheet.create({
         textAlign: 'center',
         alignItems: 'center'
     },
-    backButton: {
-        left: 60
-    },
-    border: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        textAlign: 'center',
-        borderColor: 'black',
-        top: 40,
-        width: 370,
-        height: 467,
-        backgroundColor: '#fff',
-        borderRadius: 20,
-        left: 40
+    header: {
+        width: 420,
+        height: 400,
+        borderRadius: 30,
+        top: -16,
+        backgroundColor: theme.colors.color2,
+        shadowOpacity: 0.4,
+        shadowOffset: { width: 1, height: 1 },
     },
     avatar: {
         width: 100,
         height: 100,
-        borderRadius: 13,
-        borderWidth: 2.5,
-        borderColor: (theme.colors.heading),
-        marginBottom: 90,
-        top: -8
-    },
-    viewGivenName: {
-        width: 310,
-        height: 50,
-        backgroundColor: '#333333',
-        justifyContent: 'center',
-        borderRadius: 7,
-        top: -80,
-        shadowColor: "#1a1a1a",
-        shadowOpacity: 0.6,
-        shadowRadius: 17,
-        left: -7
-    },
-    givenName: {
-        fontSize: 26.5,
-        fontFamily: (theme.fonts.title500),
-        textAlign: 'center',
-        color: (theme.colors.heading)
-    },
-    name: {
-        justifyContent: 'center',
-        top: -60,
-        width: 300,
-        height: 45,
-        backgroundColor: (theme.colors.google),
-        borderRadius: 15,
-        left: -7
-    },
-    familyName: {
-        justifyContent: 'center',
-        top: -50,
-        width: 300,
-        height: 45,
-        backgroundColor: (theme.colors.google),
-        borderRadius: 15,
-        left: -7
-    },
-    email: {
-        justifyContent: 'center',
-        top: -40,
-        width: 300,
-        height: 45,
-        backgroundColor: (theme.colors.google),
-        borderRadius: 15,
-        left: -7
-    },
-    location: {
-        justifyContent: 'center',
-        top: -30,
-        width: 300,
-        height: 45,
-        backgroundColor: (theme.colors.google),
-        borderRadius: 15,
-        left: -7
-    },
-    textName: {
-        fontSize: 14,
-        fontFamily: (theme.fonts.title500),
-        margin: 8,
-        color: (theme.colors.heading)
-    },
-    textFamilyName: {
-        fontSize: 14,
-        fontFamily: (theme.fonts.title500),
-        margin: 8,
-        color: (theme.colors.heading)
-    },
-    textEmail: {
-        fontSize: 14,
-        fontFamily: (theme.fonts.title500),
-        margin: 8,
-        color: (theme.colors.heading)
-    },
-    textLocation: {
-        fontSize: 14,
-        fontFamily: (theme.fonts.title500),
-        margin: 8,
-        color: (theme.colors.heading)
-    },
+        zIndex: 1
+    }
 });
