22fb0de89f5c5ae281ef74810f80cae1803a185f
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-08-31 14:59:41 -0300

    fix: creating the routes of the categories of the videos

---
+++ src/screens/FIIS_Videos/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { styles } from "./styles";
+import { SafeAreaView, Text } from "react-native";
+
+import { Header } from "../../components/header";
+import { Baseboard } from "../../components/baseboard";
+import { ModalPattern } from "../../components/modalPattern";
+import { BoxVideos } from "../../components/boxVideos";
+
+interface IVideosProps {
+  token: string;
+}
+
+export function Videos({ token }: IVideosProps) {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Header token={token} />
+
+      <SafeAreaView style={styles.viewMenuModal}>
+        <Text style={styles.title}>Vídeos </Text>
+        <ModalPattern
+          text={"Aqui você consegue ver vídeos sobre o que você está procurando!"}
+        />
+      </SafeAreaView>
+      <BoxVideos/>
+      
+      <SafeAreaView style={styles.boxVideo}>
+        <SafeAreaView style={styles.boxStart}>
+          <Text style={styles.start}>COMECE AQUI! </Text>
+        </SafeAreaView>
+      </SafeAreaView>
+
+      <Baseboard token={token} />
+    </SafeAreaView>
+  );
+}
