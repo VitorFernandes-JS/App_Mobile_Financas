73364dfe6542e5e963f4b3b8724707ef20a5d991
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-09-05 21:10:06 -0300

    fix: changes in goals

--- src/screens/Goals/index.tsx
+++ src/screens/Goals/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, Text, Image } from "react-native";
 import { styles } from "./styles";
 import { useRoute } from "@react-navigation/native";
 
@@ -7,22 +7,43 @@ import { Header } from "../../components/header";
 import { Baseboard } from "../../components/baseboard";
 import { ModalPattern } from "../../components/modalPattern";
 
+import TristeImg  from "../../assets/triste.png";
+
 interface IRouteParams {
-    token: string;
-  }
-  
+  token: string;
+}
+
 export function Goals() {
+  const route = useRoute();
 
-    const route = useRoute();
+  const { token } = route.params as IRouteParams;
 
-    const { token } = route.params as IRouteParams;
+  return (
+    <SafeAreaView style={styles.container}>
+      <Header token={token} />
+      <SafeAreaView style={styles.viewMenuModal}>
+        <Text style={styles.title}>Metas </Text>
+        <ModalPattern
+          text={
+            "Aqui você encontra as principais funcionalidades do BestFinance."
+          }
+        />
+      </SafeAreaView>
 
-    return(
-        <SafeAreaView style={styles.container}>
-            <Header token={token}/>
-            <ModalPattern text="Essa ferramenta te ajuda a controlar suas principais metas!"/>
-                <Text>teste</Text>
-            <Baseboard token={token}/>
-        </SafeAreaView>
-    )
+          <SafeAreaView style={styles.body}>
+            <Text style={styles.textInBody}>Você ainda não {`\n`}
+                tem metas!</Text>
+            <Image
+            style={styles.image}
+            source={TristeImg}
+            />
+          </SafeAreaView>
+
+          <SafeAreaView style={styles.add}>
+
+          </SafeAreaView>
+
+      <Baseboard token={token} />
+    </SafeAreaView>
+  );
 }
--- src/screens/Goals/styles.ts
+++ src/screens/Goals/styles.ts
@@ -13,6 +13,32 @@ export const styles = StyleSheet.create({
     color: theme.colors.color4,
   },
   viewMenuModal: {
-    flexDirection: 'row',
+    flexDirection: "row",
+  },
+  body: {
+    width: 250,
+    height: 90,
+    backgroundColor: theme.colors.color2,
+    borderRadius: 15,
+    justifyContent: "center",
+    alignItems: "center",
+    top: 200,
+    shadowOpacity: 0.3,
+    shadowRadius: 8.5,
+  },
+  textInBody: {
+    fontFamily: theme.fonts.font5_medium,
+    fontSize: 25,
+    color: theme.colors.color6,
+    top: 15,
+  },
+  image: {
+    width: 32,
+    height: 32,
+    marginLeft: 125,
+    top: -15
+  },
+  add: {
+    width: 32,
   }
 });
