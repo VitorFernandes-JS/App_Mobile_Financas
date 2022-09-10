53ff96ab62f78aa4e0b56549339f94f1912a9a8d
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-09-10 20:00:51 -0300

    fix: changes in boxCategoryVideos

--- src/components/boxCategoryVideos/index.tsx
+++ src/components/boxCategoryVideos/index.tsx
@@ -11,26 +11,24 @@ interface IBoxCategoryVideos {
 export function BoxCategoryVideos({ text }: IBoxCategoryVideos) {
   return (
     <SafeAreaView style={styles.body}>
+      <WebView
+        source={{ uri: "https://www.youtube.com/embed/t-yMB8LuzVs" }}
+        style={{ marginTop: 20 }}
+      />
 
-<WebView
-   source={{uri: 'https://www.youtube.com/embed/t-yMB8LuzVs'}}
-   style={{marginTop: 20}}
-/>
-
-
-
-      {/* <WebView
-   source={{html: '<iframe width=1000 height="70%" src="https://www.youtube.com/embed/watch?v=t-yMB8LuzVs" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'}}
-   style={{marginTop: 20}}
-/> */}
-      {/* <WebView
-          scalesPageToFit={true}
-          bounces={true}
-          javaScriptEnabled
-          style={styles.webView}
-          source={{ uri: "https://www.youtube.com/watch?v=5BTA6rL1ulE&t=24s"}}
-          automaticallyAdjustContentInsets={false}
-        /> */}
+                {/* <WebView
+            source={{html: '<iframe width=1000 height="70%" src="https://www.youtube.com/embed/watch?v=t-yMB8LuzVs" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'}}
+            style={{marginTop: 20}}
+          /> */}
+                {/* <WebView
+                    scalesPageToFit={true}
+                    bounces={true}
+                    javaScriptEnabled
+                    style={styles.webView}
+                    source={{ uri: "https://www.youtube.com/watch?v=5BTA6rL1ulE&t=24s"}}
+                    automaticallyAdjustContentInsets={false}
+                  /> */}
+                  
       <SafeAreaView style={styles.boxText}>
         <Text style={styles.text}>{text}</Text>
       </SafeAreaView>
