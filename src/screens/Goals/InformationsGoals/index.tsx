8345ca35a6449742f695f3481f6545e305665b42
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-09-12 16:31:41 -0300

    fix: changes in InformationsGoals

--- src/screens/Goals/InformationsGoals/index.tsx
+++ src/screens/Goals/InformationsGoals/index.tsx
@@ -17,8 +17,43 @@ export function InformationsGoals() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Header token={token}/>
-      <Baseboard token={token}/>
+      <Header token={token} />
+
+      {/* Campo 1*/}
+      <SafeAreaView style={styles.bodyOne}>
+        <SafeAreaView style={styles.numberOne}>
+          <Text style={styles.numberTextOne}>1</Text>
+        </SafeAreaView>
+        <SafeAreaView style={styles.fieldValueOne}>
+          <Text style={styles.fieldValueTextOne}>R${}</Text>
+        </SafeAreaView>
+      </SafeAreaView>
+
+      {/* Campo 2*/}
+      <SafeAreaView style={styles.bodyTwo}>
+        <SafeAreaView style={styles.numberTwo}>
+          <Text style={styles.numberTextTwo}>2</Text>
+        </SafeAreaView>
+        <SafeAreaView style={styles.fieldValueTwo}>
+          <Text style={styles.fieldValueTextTwo}>R${}</Text>
+        </SafeAreaView>
+      </SafeAreaView>
+
+      {/* Campo 3*/}
+      <SafeAreaView style={styles.bodyThree}>
+        <SafeAreaView style={styles.numberThree}>
+          <Text style={styles.numberTextThree}>3</Text>
+        </SafeAreaView>
+        <SafeAreaView style={styles.fieldValueThree}>
+          <Text style={styles.fieldValueTextThree}>R${}</Text>
+        </SafeAreaView>
+      </SafeAreaView>
+
+      <SafeAreaView style={styles.bodyGrafic}>
+
+      </SafeAreaView>
+
+      <Baseboard token={token} />
     </SafeAreaView>
-    );
-  }
+  );
+}
