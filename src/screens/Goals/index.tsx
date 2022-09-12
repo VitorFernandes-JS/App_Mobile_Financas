65970eea276dd264a83a3563575c91450ad6621c
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-09-11 23:50:49 -0300

    fiz: changes in Goals

--- src/screens/Goals/index.tsx
+++ src/screens/Goals/index.tsx
@@ -1,10 +1,5 @@
 import React, { useState } from "react";
-import {
-  SafeAreaView,
-  Text,
-  Image,
-  Modal
-} from "react-native";
+import { SafeAreaView, Text, Image, Modal } from "react-native";
 import { styles } from "./styles";
 import { useRoute } from "@react-navigation/native";
 import { theme } from "../../global/styles/theme";
@@ -15,6 +10,7 @@ import { ModalPattern } from "../../components/modalPattern";
 
 import TristeImg from "../../assets/triste.png";
 import AddImg from "../../assets/close.png";
+import ArrowImg from "../../assets/arrow.png";
 import { RectButton, TextInput } from "react-native-gesture-handler";
 
 import { AntDesign } from "@expo/vector-icons";
@@ -26,7 +22,8 @@ interface IRouteParams {
 export function Goals() {
   const route = useRoute();
   const { token } = route.params as IRouteParams;
-  const [modal, setModal] = useState(false);
+  const [modalPrimary, setModalPrimary] = useState(false);
+  const [modalSecondary, setModalSecondary] = useState(false);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -48,49 +45,92 @@ export function Goals() {
         <Image style={styles.image} source={TristeImg} />
       </SafeAreaView>
 
-        <RectButton
-          style={styles.add}
-          onPress={() => {
-            setModal(true);
-          }}
-        >
-        </RectButton>
-        <RectButton onPress={() => {
-            setModal(true);
-          }}>
-          <Image source={AddImg} style={styles.addImg} />
-        </RectButton>
+      <RectButton
+        style={styles.add}
+        onPress={() => {
+          setModalPrimary(true);
+        }}
+      ></RectButton>
+      <RectButton
+        onPress={() => {
+          setModalPrimary(true);
+        }}
+      >
+        <Image source={AddImg} style={styles.addImg} />
+      </RectButton>
 
-      <Modal animationType="slide" transparent={true} visible={modal}>
+      {/* Primeiro Modal */}
+      <Modal animationType="fade" transparent={true} visible={modalPrimary}>
         <SafeAreaView style={styles.viewModal}>
           <Text style={styles.titleModal}>Qual é sua meta?</Text>
 
           <SafeAreaView>
-          <TextInput 
-          style={styles.textInput1}
-          placeholder="Digite o nome da meta:"
-          placeholderTextColor={theme.colors.color4}
-          >
-          </TextInput>
+            <TextInput
+              style={styles.textInput1}
+              placeholder="Digite o nome da meta:"
+              placeholderTextColor={theme.colors.color4}
+            ></TextInput>
           </SafeAreaView>
 
           <SafeAreaView style={styles.modalPatternView}>
-          <ModalPattern text="Digite o nome da sua meta, para descrever ela de forma simples."/>
+            <ModalPattern text="Digite o nome da sua meta, para descrever ela de forma simples." />
           </SafeAreaView>
 
-          <RectButton
-            onPress={() => {
-              setModal(false);
-            }}
-            style={styles.closeModalIcon}
-          >
-            <Text></Text>
-            <AntDesign
-              name="closecircleo"
-              color="red"
-              size={23}
-              
-            />
+          <SafeAreaView style={styles.buttonLeft}>
+            <RectButton
+              onPress={() => {
+                setModalPrimary(false);
+              }}
+            >
+              <Image source={ArrowImg} style={styles.arrowImgLeft} />
+            </RectButton>
+          </SafeAreaView>
+
+          <SafeAreaView style={styles.buttonRight}>
+            <RectButton
+              onPress={() => {
+                setModalSecondary(true);
+                setModalPrimary(false);
+              }}
+            >
+              <Image source={ArrowImg} style={styles.arrowImgRight} />
+            </RectButton>
+          </SafeAreaView>
+        </SafeAreaView>
+      </Modal>
+
+      {/* Segundo Modal */}
+      <Modal animationType="fade" transparent={true} visible={modalSecondary}>
+        <SafeAreaView style={styles.viewModal}>
+          <Text style={styles.titleModal}>Quanto irá custar?</Text>
+
+          <SafeAreaView>
+            <TextInput
+              style={styles.textInput1}
+              placeholder="Digite um valor:"
+              placeholderTextColor={theme.colors.color4}
+            ></TextInput>
+          </SafeAreaView>
+
+          <SafeAreaView style={styles.modalPatternView}>
+            <ModalPattern text="Digite uma estimativa de quanto sua meta vai custar." />
+          </SafeAreaView>
+
+          <SafeAreaView style={styles.buttonLeft}>
+            <RectButton
+              onPress={() => {
+                setModalSecondary(false);
+                setModalPrimary(true);
+              }}
+            >
+              <Image source={ArrowImg} style={styles.arrowImgLeft} />
+            </RectButton>
+          </SafeAreaView>
+
+          <RectButton>
+            <SafeAreaView style={styles.buttonRight}>
+              <Image source={ArrowImg} style={styles.arrowImgRight} />
+            </SafeAreaView>
           </RectButton>
         </SafeAreaView>
       </Modal>
