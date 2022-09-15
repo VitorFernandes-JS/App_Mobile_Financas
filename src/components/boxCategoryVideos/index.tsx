7b48821045bb3cddab1358366151a3114391fa97
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-09-14 21:09:48 -0300

    fix: changes in boxCategoryVideos

--- src/components/boxCategoryVideos/index.tsx
+++ src/components/boxCategoryVideos/index.tsx
@@ -1,21 +1,26 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, Text, Image } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
 import { styles } from "./styles";
 
 // import { WebView } from "react-native-webview";
 
 interface IBoxCategoryVideos {
   text: string;
+  source: string;
 }
 
-export function BoxCategoryVideos({ text }: IBoxCategoryVideos) {
+export function BoxCategoryVideos({ text, source }: IBoxCategoryVideos) {
   return (
     <SafeAreaView style={styles.body}>
-      {/* <WebView
+      <RectButton>
+        <Image source={{ uri: require(`${source}`) }} />
+      </RectButton>
+      {/* {/* <WebView
         source={{ uri: "https://www.youtube.com/embed/t-yMB8LuzVs" }}
         style={{ marginTop: 20 }}
-      /> */}
-      <
+      /> 
+      < */}
       <SafeAreaView style={styles.boxText}>
         <Text style={styles.text}>{text}</Text>
       </SafeAreaView>
