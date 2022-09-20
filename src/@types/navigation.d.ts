a54940020d8bbd8b9c9f01532cb8e37e4ec4f536
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-09-20 19:03:47 -0300

    fix: changes in @types

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -1,29 +1,28 @@
-
 export declare global {
-    namespace ReactNavigation {
-        interface RootParamList {
-            Home: any;
-            SignIn: any;
-            Calculator: any;
-            Comparator: any;
-            Indexes: any;
-            Profile: any;
-            Wallet: any;
-            WalletInvestment: any;
-            Goals: any;
-            Financing: any;
-            Videos: any;
-            Loan: any;
-            Favorite: any;
-            FixedIncome: any;
-            Actions: any;
-            FIIS: any;
-            MobileApp: any;
-            Cripto: any;
-            Economy: any;
-            Home_Calculator: any;
-            Home_Finance: any;
-            InformationsGoals: any;
-        }
+  namespace ReactNavigation {
+    interface RootParamList {
+      Home: any;
+      SignIn: any;
+      Calculator: any;
+      Comparator: any;
+      Indexes: any;
+      Profile: any;
+      Wallet: any;
+      WalletInvestment: any;
+      Goals: any;
+      Financing: any;
+      Videos: any;
+      Loan: any;
+      Favorite: any;
+      FixedIncome: any;
+      Actions: any;
+      FIIS: any;
+      MobileApp: any;
+      Cripto: any;
+      Economy: any;
+      Home_Calculator: any;
+      Home_Finance: any;
+      InformationsGoals: any;
     }
+  }
 }
