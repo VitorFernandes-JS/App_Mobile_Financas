2dd01358bffac0cf50a827a8337b745b9ee2815a
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-09-21 12:04:52 -0300

    fix: formatting code

--- src/screens/Home_Calculator/index.tsx
+++ src/screens/Home_Calculator/index.tsx
@@ -7,42 +7,42 @@ import { useNavigation, useRoute } from "@react-navigation/native";
 import { Header } from "../../components/header";
 import { Baseboard } from "../../components/baseboard";
 
-import  CalculatorImg1  from "../../assets/predio-comercial.png";
-import  CalculatorImg2  from "../../assets/juros_compostos.png";
+import CalculatorImg1 from "../../assets/predio-comercial.png";
+import CalculatorImg2 from "../../assets/juros_compostos.png";
 
 interface IRouteParams {
-    token: string;
-  }
+  token: string;
+}
 
 export function Home_Calculator() {
-    const route = useRoute();
-    const { token } = route.params as IRouteParams;
-    const navigation = useNavigation();
+  const route = useRoute();
+  const { token } = route.params as IRouteParams;
+  const navigation = useNavigation();
 
-    function handleCalculator() {
-        navigation.navigate("Calculator", { token });
-    }
+  function handleCalculator() {
+    navigation.navigate("Calculator", { token });
+  }
 
   return (
     <SafeAreaView style={styles.container}>
-      <Header token={token}/>
-        <Text style={styles.title}>Escolha: </Text>
+      <Header token={token} />
+      <Text style={styles.title}>Escolha: </Text>
 
-        <RectButton style={styles.iconCalculator1}>
-            <Image source={CalculatorImg1} style={styles.icon} />
-            <Text style={styles.textCalculator}>
-                Calculadora {`\n`}
-                de FIIS
-            </Text>
-        </RectButton>
+      <RectButton style={styles.iconCalculator1}>
+        <Image source={CalculatorImg1} style={styles.icon} />
+        <Text style={styles.textCalculator}>
+          Calculadora {`\n`}
+          de FIIS
+        </Text>
+      </RectButton>
 
-        <RectButton style={styles.iconCalculator2} onPress={handleCalculator}>
-            <Image source={CalculatorImg2} style={styles.icon}/>
-            <Text style={styles.textCalculator}>
-                Calculadora {`\n`}
-                de Juros
-            </Text>
-        </RectButton>
+      <RectButton style={styles.iconCalculator2} onPress={handleCalculator}>
+        <Image source={CalculatorImg2} style={styles.icon} />
+        <Text style={styles.textCalculator}>
+          Calculadora {`\n`}
+          de Juros
+        </Text>
+      </RectButton>
 
       <Baseboard token={token} />
     </SafeAreaView>
