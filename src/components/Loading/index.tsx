e6b4f6e09766f9b4342fb1e939f6f97bd7b262ce
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-09-21 11:57:16 -0300

    fix: formatting code

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import { ActivityIndicator, SafeAreaView } from 'react-native';
-import { styles } from './styles';
-import { theme } from '../../global/styles/theme';
-
+import React from "react";
+import { ActivityIndicator, SafeAreaView } from "react-native";
+import { styles } from "./styles";
+import { theme } from "../../global/styles/theme";
 
 export function Loading() {
   return (
     <SafeAreaView style={styles.container}>
-        <ActivityIndicator color={theme.colors.color2}/>
+      <ActivityIndicator color={theme.colors.color2} />
     </SafeAreaView>
   );
 }
