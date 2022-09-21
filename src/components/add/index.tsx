3b387cb6722a02c261d9e8257725b929649c5b1a
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-09-21 11:52:32 -0300

    fix: formatting code

--- src/components/add/index.tsx
+++ src/components/add/index.tsx
@@ -24,19 +24,19 @@ interface IAddProps {
 
 export function Add({ setTransactionsWallets }: IAddProps) {
   const [visible, setVisible] = useState(false);
-  const [value, setValue] = useState('')
-  const [category, setCategory] = useState('')
-  const [description, setDescription] = useState('')
+  const [value, setValue] = useState("");
+  const [category, setCategory] = useState("");
+  const [description, setDescription] = useState("");
 
   return (
     <SafeAreaView style={styles.container}>
       <Modal animationType="slide" transparent={true} visible={visible}>
         <SafeAreaView style={styles.viewModal}>
-          <TextField 
-            setValue={setValue} 
-            setCategory={setCategory} 
+          <TextField
+            setValue={setValue}
+            setCategory={setCategory}
             category={category}
-            setDescription={setDescription} 
+            setDescription={setDescription}
           />
 
           <SafeAreaView style={styles.line}></SafeAreaView>
@@ -58,9 +58,14 @@ export function Add({ setTransactionsWallets }: IAddProps) {
               setTransactionsWallets((prevState) => {
                 return [
                   ...prevState,
-                  {id: `'${new Date(), (Math.random() * 100)}'`, value: Number(value), category, description}
-                ] 
-              })
+                  {
+                    id: `'${(new Date(), Math.random() * 100)}'`,
+                    value: Number(value),
+                    category,
+                    description,
+                  },
+                ];
+              });
               setVisible(false);
             }}
           >
@@ -75,7 +80,6 @@ export function Add({ setTransactionsWallets }: IAddProps) {
             <ModalWallet text="Digite as informações para adicionar um gasto ou um ganho em sua carteira!" />
           </SafeAreaView>
         </SafeAreaView>
-        
       </Modal>
 
       <RectButton
