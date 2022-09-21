6d169cb24f31d69ffaee476343cbf2104c453abb
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-09-21 12:05:03 -0300

    fix: formatting code

--- src/screens/Home_Finance/index.tsx
+++ src/screens/Home_Finance/index.tsx
@@ -11,38 +11,38 @@ import HomeImg from "../../assets/casa.png";
 import CarImg from "../../assets/carro.png";
 
 interface IRouteParams {
-    token: string;
-  }
+  token: string;
+}
 
 export function Home_Finance() {
-    const route = useRoute();
-    const { token } = route.params as IRouteParams;
-    const navigation = useNavigation();
+  const route = useRoute();
+  const { token } = route.params as IRouteParams;
+  const navigation = useNavigation();
 
-    function handleFinancing() {
-        navigation.navigate("Financing", { token });
-    }
+  function handleFinancing() {
+    navigation.navigate("Financing", { token });
+  }
 
   return (
     <SafeAreaView style={styles.container}>
-      <Header token={token}/>
-        <Text style={styles.title}>Escolha: </Text>
+      <Header token={token} />
+      <Text style={styles.title}>Escolha: </Text>
 
-        <RectButton style={styles.iconCalculator1}>
-            <Image source={HomeImg} style={styles.icon} />
-            <Text style={styles.textCalculator}>
-                Financiamento {`\n`}
-                Residencial
-            </Text>
-        </RectButton>
+      <RectButton style={styles.iconCalculator1}>
+        <Image source={HomeImg} style={styles.icon} />
+        <Text style={styles.textCalculator}>
+          Financiamento {`\n`}
+          Residencial
+        </Text>
+      </RectButton>
 
-        <RectButton style={styles.iconCalculator2} onPress={handleFinancing}>
-            <Image source={CarImg} style={styles.icon}/>
-            <Text style={styles.textCalculator}>
-                Financiamento {`\n`}
-                Veicular
-            </Text>
-        </RectButton>
+      <RectButton style={styles.iconCalculator2} onPress={handleFinancing}>
+        <Image source={CarImg} style={styles.icon} />
+        <Text style={styles.textCalculator}>
+          Financiamento {`\n`}
+          Veicular
+        </Text>
+      </RectButton>
 
       <Baseboard token={token} />
     </SafeAreaView>
