74f22a0f6300eaf326274ecddadf9bb71e64425b
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-09-21 12:03:44 -0300

    fix: formatting code

--- src/screens/FixedIncome_Videos/index.tsx
+++ src/screens/FixedIncome_Videos/index.tsx
@@ -12,24 +12,21 @@ interface IVideosProps {
 }
 
 export function FixedIncome({ token }: IVideosProps) {
-
   return (
     <SafeAreaView style={styles.container}>
       <Header token={token} />
 
       <SafeAreaView style={styles.viewMenuModal}>
         <Text style={styles.title}>Renda Fixa </Text>
-        <ModalPattern
-          text={"Aqui você encontra vídeos sobre renda fixa!"}
-        />
+        <ModalPattern text={"Aqui você encontra vídeos sobre renda fixa!"} />
       </SafeAreaView>
 
       <SafeAreaView style={styles.viewBoxCategoryVideos}>
-        <BoxCategoryVideos text="Video 1" source={Cover_Fixed_Income[0]}/>
-        <BoxCategoryVideos text="Video 2" source={Cover_Fixed_Income[1]}/>
-        <BoxCategoryVideos text="Video 3" source={Cover_Fixed_Income[2]}/>
-        <BoxCategoryVideos text="Video 4" source={Cover_Fixed_Income[3]}/>
-        <BoxCategoryVideos text="Video 5" source={Cover_Fixed_Income[4]}/>
+        <BoxCategoryVideos text="Video 1" source={Cover_Fixed_Income[0]} />
+        <BoxCategoryVideos text="Video 2" source={Cover_Fixed_Income[1]} />
+        <BoxCategoryVideos text="Video 3" source={Cover_Fixed_Income[2]} />
+        <BoxCategoryVideos text="Video 4" source={Cover_Fixed_Income[3]} />
+        <BoxCategoryVideos text="Video 5" source={Cover_Fixed_Income[4]} />
       </SafeAreaView>
 
       <Baseboard token={token} />
