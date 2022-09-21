7d37144bfc3fd22d7e0b02bb1c9fd30b5fdfaff8
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-09-21 11:58:45 -0300

    fix: formatting code

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -5,19 +5,19 @@ export const theme = {
     color3: "#a0a0a0",
     color4: "#717f7f",
     color5: "#f1f5f4",
-    color6: "#ffffff"
+    color6: "#ffffff",
   },
 
   fonts: {
     // fonte Raleway
-    font1_thin: "Raleway_100Thin", 
-    font2_extraLight: "Raleway_200ExtraLight", 
-    font3_light: "Raleway_300Light", 
-    font4_regular: "Raleway_400Regular", 
-    font5_medium: "Raleway_500Medium", 
-    font6_semiBold: "Raleway_600SemiBold", 
-    font7_bold: "Raleway_700Bold", 
-    font8_extraBold: "Raleway_800ExtraBold", 
+    font1_thin: "Raleway_100Thin",
+    font2_extraLight: "Raleway_200ExtraLight",
+    font3_light: "Raleway_300Light",
+    font4_regular: "Raleway_400Regular",
+    font5_medium: "Raleway_500Medium",
+    font6_semiBold: "Raleway_600SemiBold",
+    font7_bold: "Raleway_700Bold",
+    font8_extraBold: "Raleway_800ExtraBold",
     font9_black: "Raleway_900Black",
     // fonte Inter
     font1_thin_inter: "Inter_100Thin",
