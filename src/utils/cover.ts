aec5dea41b98cfe79cdb73b5ee454f9ab6a87864
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-10-27 12:07:32 -0300

    feat: add title in JSON (cover)

--- src/utils/cover.ts
+++ src/utils/cover.ts
@@ -1,19 +1,23 @@
 export const Cover_Fixed_Income = [
   {
     cover: require("../assets/capas_renda_fixa/capa1.png"),
-    title: "Tesouro Selic",
+    title: "Como fazer Renda Extra na Internet!",
   },
   {
     cover: require("../assets/capas_renda_fixa/capa2.jpg"),
+    title: "3 formas de ganhar dinheiro extra!",
   },
   {
     cover: require("../assets/capas_renda_fixa/capa3.jpg"),
+    title: "Como ganhar MUITO DINHEIRO com Axie Infinity!",
   },
   {
     cover: require("../assets/capas_renda_fixa/capa4.jpg"),
+    title: "Qual banco digital rende mais?"
   },
   {
     cover: require("../assets/capas_renda_fixa/capa5.jpg"),
+    title: "APPS que pagam para você usar!"
   },
 ];
 
@@ -22,87 +26,112 @@ export const Cover_Fixed_Income = [
 export const Cover_Actions = [
   {
     cover: require("../assets/capas_acoes/capa1.jpg"),
+    title: "O que fazer com seu primeiro R$ 1.000,00?",
   },
   {
     cover: require("../assets/capas_acoes/capa2.jpg"),
+    title: "5 hábitos dos milionários!",
   },
   {
     cover: require("../assets/capas_acoes/capa3.jpg"),
+    title: "Como investir em ações?",
   },
   {
     cover: require("../assets/capas_acoes/capa4.jpg"),
+    title: "Como investir em ações com pouco dinheiro?"
   },
   {
     cover: require("../assets/capas_acoes/capa5.jpg"),
+    title: "Investi R$ 130,00 na prática!"
   },
   {
     cover: require("../assets/capas_acoes/capa6.jpg"),
+    title: "Explicando termos de investimento!"
   },
   {
     cover: require("../assets/capas_acoes/capa7.jpg"),
+    title: "Estou perdendo dinheiro investindo?"
   },
   {
     cover: require("../assets/capas_acoes/capa8.jpg"),
+    title: "Do R$ 0 aos R$ 50.000!"
   },
   {
     cover: require("../assets/capas_acoes/capa9.jpg"),
+    title: "Como tranferir ações entre corretoras?"
   },
   {
     cover: require("../assets/capas_acoes/capa10.jpg"),
+    title: "Investi todo meu décimo terceiro!"
   },
   {
     cover: require("../assets/capas_acoes/capa11.jpg"),
+    title: "Receba dividentos todos os meses do ano!"
   },
 ];
 
 export const Cover_FIIS = [
   {
     cover: require("../assets/capas_fiis/capa1.jpg"),
+    title: "Investindo em FIIS na prática!",
   },
   {
     cover: require("../assets/capas_fiis/capa2.jpg"),
+    title: "O que são FIIs?",
   },
 ];
 
 export const Cover_Criptos = [
   {
     cover: require("../assets/capas_criptos/capa1.jpg"),
+    title: "Comprando Bitcoin na prática!",
   },
   {
     cover: require("../assets/capas_criptos/capa2.jpg"),
+    title: "Nova criptomoeda brasileira!"
   },
   {
     cover: require("../assets/capas_criptos/capa3.jpg"),
+    title: "Explicando o que é uma criptomoeda!"
   },
   {
     cover: require("../assets/capas_criptos/capa4.jpg"),
+    title: "Criptos que vão bombar em 2022!"
   },
 ];
 
 export const Cover_Economy = [
   {
     cover: require("../assets/capas_economia/capa1.jpg"),
+    title: "Essa é a familia mais rica do mundo!",
   },
   {
     cover: require("../assets/capas_economia/capa2.jpg"),
+    title: "Essa é a moeda mais cara do MUNDO!"
   },
   {
     cover: require("../assets/capas_economia/capa3.jpg"),
+    title: "A escola nunca te ensinou isso!"
   },
   {
     cover: require("../assets/capas_economia/capa4.jpg"),
+    title: "Qual o melhor cartão que existe?"
   },
   {
     cover: require("../assets/capas_economia/capa5.jpg"),
+    title: "Por que está tudo tão caro no Brasil?"
   },
   {
     cover: require("../assets/capas_economia/capa6.jpg"),
+    title: "Como baixar sua conta de luz agora mesmo!"
   },
   {
     cover: require("../assets/capas_economia/capa7.jpg"),
+    title: "Crise de 1929 (Documentário)"
   },
   {
     cover: require("../assets/capas_economia/capa8.jpg"),
+    title: "Tenha ENERGIA DE GRAÇA!"
   },
 ];
 
