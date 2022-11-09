dd10967b8b105a98a5722c0918daa7a5c100dc1c
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-11-09 13:42:54 -0300

    feat: (WARNING) change SDK 45.0.0

--- src/components/boxCategoryVideos/index.tsx
+++ src/components/boxCategoryVideos/index.tsx
@@ -1,7 +1,6 @@
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { SafeAreaView, Text, Image, TouchableOpacity, TouchableOpacityProps } from "react-native";
-import { RectButton } from "react-native-gesture-handler";
 import { styles } from "./styles";
 
 interface IBoxCategoryVideos extends TouchableOpacityProps {
