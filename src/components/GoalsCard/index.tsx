ebed9495ca66a87808ad0446999a716b10b0bfdb
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-11-09 23:51:02 -0300

    fix: changes in InformationsGoals

--- src/components/GoalsCard/index.tsx
+++ src/components/GoalsCard/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
-import { styles } from './styles';
+import React, { useState } from "react";
+import { Modal, SafeAreaView, Text } from "react-native";
+import { styles } from "./styles";
 
-import { RectButton } from 'react-native-gesture-handler';
-import { useNavigation, useRoute } from '@react-navigation/native';
-import { Trash } from '../trash';
-import { apiFinances } from '../../services';
+import { BorderlessButton, RectButton } from "react-native-gesture-handler";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { Trash } from "../trash";
+import { apiFinances } from "../../services";
 export interface GoalsCardProps {
   number: any;
   name: string;
@@ -22,7 +22,14 @@ interface IRouteParams {
   token: string;
 }
 
-export function GoalsCard({ number, name, amount, id, setData }: GoalsCardProps) {
+export function GoalsCard({
+  number,
+  name,
+  amount,
+  id,
+  setData,
+}: GoalsCardProps) {
+  const [visible, setVisible] = useState(false);
   const navigation = useNavigation();
   const route = useRoute();
   const { token } = route.params as IRouteParams;
@@ -32,14 +39,23 @@ export function GoalsCard({ number, name, amount, id, setData }: GoalsCardProps)
   }
 
   function handleDeleteGoal() {
-    apiFinances.delete('/goals/' + id).then(() => {
-      apiFinances.get("/goals").then((response) => {
-        setData(response?.data.sort((a: any, b: any) => +new Date(a.created_at) - +new Date(b.created_at)));
+    apiFinances
+      .delete("/goals/" + id)
+      .then(() => {
+        apiFinances.get("/goals").then((response) => {
+          setData(
+            response?.data.sort(
+              (a: any, b: any) =>
+                +new Date(a.created_at) - +new Date(b.created_at)
+            )
+          );
+        });
       })
-    }).catch((err) => console.log(err))
+      .catch((err) => console.log(err));
   }
 
   return (
+    <SafeAreaView>
     <RectButton style={styles.body} onPress={handleWalletInvestment}>
       <SafeAreaView style={styles.number}>
         <Text style={styles.numberText}>{number}</Text>
@@ -48,11 +64,48 @@ export function GoalsCard({ number, name, amount, id, setData }: GoalsCardProps)
         <Text style={styles.title}>{name}</Text>
       </SafeAreaView>
       <SafeAreaView style={styles.fieldValue}>
-        <Text style={styles.fieldValueText}>{new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL', maximumFractionDigits: 2 }).format(amount)}</Text>
+        <Text style={styles.fieldValueText}>
+          {new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+            maximumFractionDigits: 2,
+          }).format(amount)}
+        </Text>
       </SafeAreaView>
       <SafeAreaView style={styles.viewTrash}>
-        <Trash onPress={handleDeleteGoal} />
+        <Trash
+          onPress={() => {
+            setVisible(true);
+          }}
+        />
       </SafeAreaView>
     </RectButton>
+
+    <Modal animationType="slide" transparent={true} visible={visible}>
+        <SafeAreaView style={styles.viewModal}>
+          <Text style={styles.modalSubtitle}>Tem certeza dessa exclusão?</Text>
+        </SafeAreaView>
+
+        <SafeAreaView style={styles.viewButtons}>
+          <BorderlessButton
+            style={styles.buttonToExclude}
+            onPress={handleDeleteGoal}
+          >
+            <Text style={styles.textToExclude}>Excluir</Text>
+          </BorderlessButton>
+
+          <BorderlessButton
+            style={styles.buttonToCancel}
+            onPress={() => {
+              setVisible(false);
+            }}
+          >
+            <Text style={styles.textToCancel}>Cancelar</Text>
+          </BorderlessButton>
+        </SafeAreaView>
+        <Text style={styles.textBaseboard}>Obs: isto é permanente!</Text>
+      </Modal>
+
+    </SafeAreaView>
   );
 }
